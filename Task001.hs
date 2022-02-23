243e173d612e76734c15c07c5126dcde691e4f4c
Krille1994 <Kristian.risvaer@gmail.com>
2022-02-23 12:09:00 +0100

    tidying up Task001

--- Task001.hs
+++ Task001.hs
@@ -1,13 +1,17 @@
 import Data.List
 
 ordAndGroupList :: String -> [String]
-ordAndGroupList [] = []
-ordAndGroupList str = group (sort str)
+--ordAndGroupList [] = []
+ordAndGroupList str = group $ sort str
 
 createPrintLine :: String -> String
 createPrintLine [] = []
 createPrintLine a = head a : " : " ++ replicate (length a) '#' ++ "\n"
 
 printFreq :: String -> String
-printFreq [] = []
-printFreq xs = concatMap createPrintLine (ordAndGroupList xs)
+--printFreq [] = []
+printFreq xs = concatMap createPrintLine $ ordAndGroupList xs
+
+-- map :: (a -> b) -> [a] -> [b]
+-- map _ [] = []          This is why line 12 is unnessessary
+-- map f (x:xs) = f x : map f xs
